d57812dbda168ddd134ed9987aea31b11df2c67b
vJalalv <galalramadan146@gmail.com>
2025-05-09 01:11:14 +0300

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,21 +1,23 @@
 function validateForm() {
-    let name =document.getElementById("name").value;
-    let email =document.getElementById("email").value;
-    let phone =document.getElementById("phone").value;
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
 
-if(name === "" || email === "" || message === "") {
-    alert("Please fill in all information")
-    return false;
-}
-if(!email.includes("@")) {
-    alert("Please enter a valid email address");
-    return false;
+  if(name === "" || email === "" || message === "") {
+      alert("Please fill in all information");
+      return false;
   }
+
+  if(!email.includes("@")) {
+      alert("Please enter a valid email address");
+      return false;
+  }
+
   alert("Your message has been sent successfully.");
   return true;
 }
+
 function saveMessage() {
-    let message = document.getElementById("message").value;
-    localStorage.setItem("lastMessage", message);
-  }
-  
+  let message = document.getElementById("message").value;
+  localStorage.setItem("lastMessage", message);
+}
